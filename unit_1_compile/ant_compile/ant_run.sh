cac6273aa98cab2865c8a093e40b2adaf7762be0
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-03-05 18:46:42 +0200

    add an ant home variable

--- unit_1_compile/ant_compile/ant_run.sh
+++ unit_1_compile/ant_compile/ant_run.sh
@@ -1,3 +1,11 @@
+echo SET ANT ENVIRONMENT
+cd ../ant
+. ./setantenv.sh
+echo CHECK ANT HOME VARIABLE
+echo  ANT_HOME: $ANT_HOME
+
+cd ../ant_compile
+
 echo ANT COMPILE OUTPUT:
 ant clean
 ant compile
