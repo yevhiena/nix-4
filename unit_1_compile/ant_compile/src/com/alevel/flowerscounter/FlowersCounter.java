3800acaccc0fb7fd04c9d53b97fad0ddcf58808b
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-03-05 16:49:51 +0200

    add ant and terminal compile

---
+++ unit_1_compile/ant_compile/src/com/alevel/flowerscounter/FlowersCounter.java
@@ -0,0 +1,13 @@
+package com.alevel.flowerscounter;
+
+import com.alevel.flowers.Flowers;
+import org.apache.commons.collections4.MapUtils;
+
+
+public class FlowersCounter {
+    public void printAmountOfFlowers(){
+        Flowers fl =new Flowers();
+        int amountOfFlowers = MapUtils.size(fl.getFlowers()) ;
+        System.out.println("MAVEN PROJ OUTPUT: There are "+ amountOfFlowers + " flowers in store");
+    }
+}
