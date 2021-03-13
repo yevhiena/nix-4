20d36afb2c77c935b4c0a2af2194be05e989e978
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-03-13 11:01:38 +0200

    digits

--- unit_2_algorithmic_tasks/app/src/main/java/ua/com/alevel/SumOfNumbersInRandomString.java
+++ unit_2_algorithmic_tasks/app/src/main/java/ua/com/alevel/SumOfNumbersInRandomString.java
@@ -9,12 +9,11 @@ public class SumOfNumbersInRandomString {
 
     public static int sumOfNumbers(String string){
         int sum = 0;
-        Pattern pattern = Pattern.compile("\\d+");
+        Pattern pattern = Pattern.compile("\\d");
         Matcher matcher = pattern.matcher(string);
         while (matcher.find()) {
             sum += Integer.parseInt(matcher.group());
         }
-        System.out.println("Notice: counting the sum of numbers intentionally, not digits");
         return sum;
     }
 
