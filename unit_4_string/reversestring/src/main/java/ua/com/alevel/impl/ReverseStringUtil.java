8b4b3ddc80089be5ddb5cdffbbcadc2041d26d31
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-03-19 20:03:13 +0200

    add StringUtils to custom lib

---
+++ unit_4_string/reversestring/src/main/java/ua/com/alevel/impl/ReverseStringUtil.java
@@ -0,0 +1,36 @@
+package ua.com.alevel.impl;
+
+
+
+
+public class ReverseStringUtil {
+
+    public static String reverse(String src) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = src.length() - 1; i>=0; i--){
+            stringBuilder.append(src.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+
+
+    public static String reverse(String src, String dest){
+        StringBuilder stringBuilder = new StringBuilder(src);
+
+        int indexOfSubString = stringBuilder.indexOf(dest);
+        stringBuilder.replace(indexOfSubString, indexOfSubString + dest.length(), reverse(dest));
+
+        return stringBuilder.toString();
+    }
+
+
+    public static String reverse(String src, int firstIndex, int lastIndex) {
+        StringBuilder stringBuilder = new StringBuilder(src);
+        char[] array = new char [lastIndex - firstIndex + 1];
+        stringBuilder.getChars(firstIndex, lastIndex + 1, array, 0);
+        stringBuilder.replace(firstIndex, lastIndex + 1 , reverse(String.copyValueOf(array)));
+
+        return stringBuilder.toString();
+    }
+}
