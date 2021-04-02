c9024a3451a200f8d7e6c5aa2a991ea3a1d387bd
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-04-02 17:06:55 +0300

    add classes to run programm

---
+++ unit_5_oop/src/main/java/ua/com/alevel/CrudMain.java
@@ -0,0 +1,12 @@
+package ua.com.alevel;
+
+import ua.com.alevel.view.View;
+
+public class CrudMain {
+    public static void main(String[] args) throws Exception {
+
+        View.view();
+    }
+}
+
+
