6989762d433587c35f93ddb24e3fb0ebaf1ec8b2
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-04-20 20:05:26 +0300

    add main

---
+++ unit_9_ionio/src/main/java/ua/com/alevel/IONIOMain.java
@@ -0,0 +1,11 @@
+package ua.com.alevel;
+
+import ua.com.alevel.view.View;
+
+public class IONIOMain {
+    public static void main(String[] args) throws Exception {
+        View.view();
+    }
+}
+
+
