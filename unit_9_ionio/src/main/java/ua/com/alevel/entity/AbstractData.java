6e3ddb744340762f266a44603226df2f99056a0b
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-04-20 20:04:22 +0300

    add data classes

---
+++ unit_9_ionio/src/main/java/ua/com/alevel/entity/AbstractData.java
@@ -0,0 +1,11 @@
+package ua.com.alevel.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AbstractData {
+    protected int id;
+    protected boolean visible = true;
+}
---
+++ unit_9_ionio/src/main/java/ua/com/alevel/entity/Author.java
@@ -0,0 +1,21 @@
+package ua.com.alevel.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Author extends AbstractData{
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                " id='" + id + '\'' +
+                ", first name='" + firstName + '\'' +
+                ", last name='" + lastName + '\'' +
+                '}';
+    }
+
+}
