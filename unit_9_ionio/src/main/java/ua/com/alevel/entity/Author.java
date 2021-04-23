ee926c0b3f55bf5122a5607e96a7edfaf4fcc02e
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-04-23 18:05:14 +0300

    add equals and hashcode

--- unit_9_ionio/src/main/java/ua/com/alevel/entity/Author.java
+++ unit_9_ionio/src/main/java/ua/com/alevel/entity/Author.java
@@ -1,10 +1,12 @@
 package ua.com.alevel.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@EqualsAndHashCode
 public class Author extends AbstractData{
     private String firstName;
     private String lastName;
--- unit_9_ionio/src/main/java/ua/com/alevel/entity/Book.java
+++ unit_9_ionio/src/main/java/ua/com/alevel/entity/Book.java
@@ -1,30 +1,18 @@
 package ua.com.alevel.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Objects;
 
 
 @Setter
 @Getter
+@EqualsAndHashCode
 public class Book extends AbstractData{
     private String title;
     private List<Integer> authorId;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(title, book.title);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title);
-    }
 
     @Override
     public String toString() {
