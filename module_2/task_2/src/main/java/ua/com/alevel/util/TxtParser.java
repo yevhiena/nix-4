109b7cf9498a0a240980742f605a2e391a4faef7
Yevheniia Honcharenko <eugenea.gonch@gmail.com>
2021-05-15 02:48:02 +0300

    add task 2

---
+++ module_2/task_2/src/main/java/ua/com/alevel/util/TxtParser.java
@@ -0,0 +1,14 @@
+package ua.com.alevel.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TxtParser {
+
+    public static List<String> readData(String path) throws IOException {
+
+        return  Files.readAllLines(Paths.get(path));
+    }
+}
